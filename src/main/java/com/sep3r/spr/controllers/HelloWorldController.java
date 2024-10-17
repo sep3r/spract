53583e9e1f56e72431681ae04a0f9ad35cf8ee25
sep3r <sep3r@yahoo.com>
2024-10-17 20:49:47 +0330

    feat(helloworld): added getStudent and getStudents into hello world controller for testing api working fine

--- src/main/java/com/sep3r/spr/controllers/HelloWorldController.java
+++ src/main/java/com/sep3r/spr/controllers/HelloWorldController.java
@@ -1,13 +1,27 @@
 package com.sep3r.spr.controllers;
 
+import com.sep3r.spr.entities.StudentEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Struct;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello-world")
-    public String helloWorlWorld() {
-        return "Hello World!";
+    @GetMapping("/student")
+    public StudentEntity helloWorlWorld() {
+       return new StudentEntity(1L, "sepehr", "mirzanezhad fard");
+    }
+
+    @GetMapping("/students")
+    public List<StudentEntity> getAllStudents() {
+        List<StudentEntity> students = new ArrayList<>();
+        students.add(new StudentEntity(1L, "sepehr", "mirzanezhad fard"));
+        students.add(new StudentEntity(2L, "sahar", "mirzanezhad fard"));
+        students.add(new StudentEntity(3L, "mona", "zarei"));
+        return students;
     }
 }
