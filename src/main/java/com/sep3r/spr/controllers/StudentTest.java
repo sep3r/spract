6b540d385a372218a3fa0ee2c859447898633a4a
sep3r <sep3r@yahoo.com>
2024-10-17 21:20:37 +0330

    feat(student-test): added HttpEntity for response

--- src/main/java/com/sep3r/spr/controllers/StudentTest.java
+++ src/main/java/com/sep3r/spr/controllers/StudentTest.java
@@ -1,6 +1,7 @@
 package com.sep3r.spr.controllers;
 
 import com.sep3r.spr.entities.StudentEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,19 +9,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class HelloWorldController {
+public class StudentTest {
 
-    @GetMapping("/student")
-    public StudentEntity helloWorlWorld() {
+    @GetMapping("student")
+    public StudentEntity getStudent() {
        return new StudentEntity(1L, "sepehr", "mirzanezhad fard");
     }
 
-    @GetMapping("/students")
+    @GetMapping("students")
     public List<StudentEntity> getAllStudents() {
         List<StudentEntity> students = new ArrayList<>();
         students.add(new StudentEntity(1L, "sepehr", "mirzanezhad fard"));
         students.add(new StudentEntity(2L, "sahar", "mirzanezhad fard"));
         students.add(new StudentEntity(3L, "mona", "zarei"));
         return students;
+    }
+
+    @GetMapping("stu")
+    public ResponseEntity<StudentEntity> getStudentHttpEntity() {
+        return ResponseEntity.ok().body(new StudentEntity(1L, "sepehr", "mirzanezhad fard"));
     }
 }
