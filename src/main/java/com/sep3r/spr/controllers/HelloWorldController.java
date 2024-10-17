f855624fb22681cce9d8615e7c7cf94c8618a4fe
sep3r <sep3r@yahoo.com>
2024-10-17 20:50:52 +0330

    feat(helloworld): added getStudent and getStudents into hello world controller for testing api working fine

--- src/main/java/com/sep3r/spr/controllers/HelloWorldController.java
+++ src/main/java/com/sep3r/spr/controllers/HelloWorldController.java
@@ -4,7 +4,6 @@ import com.sep3r.spr.entities.StudentEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Struct;
 import java.util.ArrayList;
 import java.util.List;
 
