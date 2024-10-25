45d4cd0d4c9145b420bd23f520473bcada01d1bf
sep3r <sep3r@yahoo.com>
2024-10-25 18:08:07 +0330

    feat(student-test): added HttpEntity for response

--- src/main/java/com/sep3r/spr/controllers/StudentTest.java
+++ src/main/java/com/sep3r/spr/controllers/StudentTest.java
@@ -2,12 +2,14 @@ package com.sep3r.spr.controllers;
 
 import com.sep3r.spr.entities.StudentEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 public class StudentTest {
 
@@ -17,16 +19,21 @@ public class StudentTest {
     }
 
     @GetMapping("students")
-    public List<StudentEntity> getAllStudents() {
+    public ResponseEntity<List<StudentEntity>> getAllStudents() {
         List<StudentEntity> students = new ArrayList<>();
         students.add(new StudentEntity(1L, "sepehr", "mirzanezhad fard"));
         students.add(new StudentEntity(2L, "sahar", "mirzanezhad fard"));
         students.add(new StudentEntity(3L, "mona", "zarei"));
-        return students;
+        students.add(new StudentEntity(4L, "majid", "zarei"));
+        return ResponseEntity.ok()
+                .header("Header","sepehr-sahar")
+                .body(students);
     }
 
     @GetMapping("stu")
     public ResponseEntity<StudentEntity> getStudentHttpEntity() {
-        return ResponseEntity.ok().body(new StudentEntity(1L, "sepehr", "mirzanezhad fard"));
+        return ResponseEntity.ok()
+                .header("Header","sepehr-sahar")
+                .body(new StudentEntity(1L, "sepehr", "mirzanezhad fard"));
     }
 }
